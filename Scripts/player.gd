18bd29375b0c638799457e8e4076fe761064ff59
Mike Tran <anhminh27204@gmail.com>
2025-09-08 04:05:37 -0400

    Update movements and their animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,23 +3,37 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
+var x_direction
+var y_direction 
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	x_direction = Input.get_axis("ui_left", "ui_right")
+	y_direction = Input.get_axis("ui_up", "ui_down")
+	
+	if x_direction:
+		if (x_direction > 0): animated_sprite.play("Turnright")
+		if (x_direction < 0): animated_sprite.play("Turnleft")
+		velocity.x = x_direction * SPEED	
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+	if y_direction:
+		if (y_direction < 0): animated_sprite.play("Walkup")
+		if (y_direction > 0): animated_sprite.play("walkdown")
+		velocity.y = y_direction * SPEED
+		
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+		
 
 	move_and_slide()
